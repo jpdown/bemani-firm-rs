4668d298f4c2065d4356f9818745838006416caa
Jaden Down <24631328+jpdown@users.noreply.github.com>
2025-07-31 18:12:03 -0500

    working usb, reading one button, needs to be split into files

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,200 @@
-//! This example test the RP Pico on board LED.
-//!
-//! It does not work with the RP Pico W board. See wifi_blinky.rs.
-
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use core::sync::atomic::AtomicBool;
+use core::sync::atomic::Ordering;
+
+use defmt::info;
+use defmt::warn;
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
+use embassy_rp::bind_interrupts;
 use embassy_rp::gpio;
+use embassy_rp::gpio::Input;
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::Driver;
+use embassy_rp::usb::InterruptHandler;
 use embassy_time::Timer;
+use embassy_usb::Builder;
+use embassy_usb::Config;
+use embassy_usb::Handler;
+use embassy_usb::class::hid::HidReaderWriter;
+use embassy_usb::class::hid::ReportId;
+use embassy_usb::class::hid::RequestHandler;
+use embassy_usb::class::hid::State;
+use embassy_usb::control::OutResponse;
 use gpio::{Level, Output};
+use usbd_hid::descriptor::AsInputReport;
+use usbd_hid::descriptor::SerializedDescriptor;
+use usbd_hid::descriptor::gen_hid_descriptor;
+use usbd_hid::descriptor::generator_prelude::Serialize;
+use usbd_hid::descriptor::generator_prelude::SerializeTuple;
+use usbd_hid::descriptor::generator_prelude::Serializer;
 use {defmt_rtt as _, panic_probe as _};
+
+#[gen_hid_descriptor(
+    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = JOYSTICK) = {
+        (collection = PHYSICAL, usage = GAMEPAD) = {
+            (usage_page = BUTTON, usage_min = 0, usage_max = 8) = {
+                #[packed_bits 8] #[item_settings data,variable,absolute] buttons=input;
+            };
+            (usage_page = GENERIC_DESKTOP,) = {
+                (usage = X, logical_min = 0) = {
+                    #[iitem_settings data,variable,absolute] tt=input;
+                };
+            };
+        };
+    }
+)]
+pub struct KonamiIIDXReport {
+    pub buttons: u8,
+    pub tt: u8,
+}
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    let b1 = Input::new(p.PIN_2, gpio::Pull::Up);
 
-    loop {
-        info!("led on!");
-        led.set_high();
-        Timer::after_secs(1).await;
+    let driver = Driver::new(p.USB, Irqs);
 
-        info!("led off!");
-        led.set_low();
-        Timer::after_secs(1).await;
+    let mut config = Config::new(0x1CCF, 0x8048);
+    config.manufacturer = Some("Konami Amusement");
+    config.product = Some("beatmania IIDX controller premium model");
+    config.serial_number = Some("12345678");
+
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut msos_descriptor = [0; 256];
+    let mut control_buf = [0; 256];
+    let mut request_handler = MyRequestHandler {};
+    let mut device_handler = MyDeviceHandler::new();
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut msos_descriptor,
+        &mut control_buf,
+    );
+
+    builder.handler(&mut device_handler);
+
+    let config = embassy_usb::class::hid::Config {
+        report_descriptor: KonamiIIDXReport::desc(),
+        request_handler: None,
+        poll_ms: 1,
+        max_packet_size: 64,
+    };
+
+    let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    let (reader, mut writer) = hid.split();
+
+    let in_fut = async {
+        loop {
+            let b1_pressed = b1.is_low();
+            info!("button 1 pressed: {}", b1_pressed);
+
+            let report = KonamiIIDXReport {
+                tt: 0,
+                buttons: match b1_pressed {
+                    true => 0xFF,
+                    false => 0x00,
+                },
+            };
+
+            // Send the report.
+            match writer.write_serialize(&report).await {
+                Ok(()) => {}
+                Err(e) => warn!("Failed to send report: {:?}", e),
+            };
+        }
+    };
+
+    let out_fut = async {
+        reader.run(false, &mut request_handler).await;
+    };
+
+    join(usb_fut, join(in_fut, out_fut)).await;
+}
+
+struct MyRequestHandler {}
+
+impl RequestHandler for MyRequestHandler {
+    fn get_report(&mut self, id: ReportId, _buf: &mut [u8]) -> Option<usize> {
+        info!("Get report for {:?}", id);
+        None
+    }
+
+    fn set_report(&mut self, id: ReportId, data: &[u8]) -> OutResponse {
+        info!("Set report for {:?}: {=[u8]}", id, data);
+        OutResponse::Accepted
+    }
+
+    fn set_idle_ms(&mut self, id: Option<ReportId>, dur: u32) {
+        info!("Set idle rate for {:?} to {:?}", id, dur);
+    }
+
+    fn get_idle_ms(&mut self, id: Option<ReportId>) -> Option<u32> {
+        info!("Get idle rate for {:?}", id);
+        None
+    }
+}
+
+struct MyDeviceHandler {
+    configured: AtomicBool,
+}
+
+impl MyDeviceHandler {
+    fn new() -> Self {
+        MyDeviceHandler {
+            configured: AtomicBool::new(false),
+        }
+    }
+}
+
+impl Handler for MyDeviceHandler {
+    fn enabled(&mut self, enabled: bool) {
+        self.configured.store(false, Ordering::Relaxed);
+        if enabled {
+            info!("Device enabled");
+        } else {
+            info!("Device disabled");
+        }
+    }
+
+    fn reset(&mut self) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("Bus reset, the Vbus current limit is 100mA");
+    }
+
+    fn addressed(&mut self, addr: u8) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("USB address set to: {}", addr);
+    }
+
+    fn configured(&mut self, configured: bool) {
+        self.configured.store(configured, Ordering::Relaxed);
+        if configured {
+            info!(
+                "Device configured, it may now draw up to the configured current limit from Vbus."
+            )
+        } else {
+            info!("Device is no longer configured, the Vbus current limit is 100mA.");
+        }
     }
 }
