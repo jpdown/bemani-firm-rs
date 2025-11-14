5c536b8e20237324418d76910c94e3b61319ba0d
Jaden Down <24631328+jpdown@users.noreply.github.com>
2025-11-13 20:16:08 -0600

    use quarter steps for nicer math

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,32 +1,149 @@
+use core::num;
+
 use defmt::debug;
 use embassy_rp::Peri;
 use embassy_rp::bind_interrupts;
+use embassy_rp::clocks::clk_sys_freq;
+use embassy_rp::dma::AnyChannel;
+use embassy_rp::dma::Channel;
 use embassy_rp::peripherals::PIN_0;
 use embassy_rp::peripherals::PIN_1;
 use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::Common;
+use embassy_rp::pio::Config;
+use embassy_rp::pio::FifoJoin;
+use embassy_rp::pio::Instance;
 use embassy_rp::pio::InterruptHandler;
+use embassy_rp::pio::LoadedProgram;
+use embassy_rp::pio::Pin;
 use embassy_rp::pio::Pio;
+use embassy_rp::pio::ShiftDirection;
+use embassy_rp::pio::StateMachine;
+use embassy_rp::pio::program::pio_asm;
 use embassy_rp::pio_programs::rotary_encoder::Direction;
 use embassy_rp::pio_programs::rotary_encoder::PioEncoder;
 use embassy_rp::pio_programs::rotary_encoder::PioEncoderProgram;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::signal::Signal;
 use embassy_sync::watch::Sender;
+use fixed::traits::ToFixed;
 
-const PPR: u16 = 360;
-pub const TARGET_STEPS: u16 = 144;
+const PPR: i32 = 360 * 4;
+pub const TARGET_STEPS: i32 = 144;
 
-// TODO: make the encoder do quarter steps so this math is nicer
-const THRESHOLD: u8 = ((PPR) / gcd(PPR, TARGET_STEPS)) as u8;
-const ENCODER_STEP: u8 = (TARGET_STEPS / gcd(PPR, TARGET_STEPS)) as u8;
+const THRESHOLD: i32 = (PPR) / gcd(PPR, TARGET_STEPS);
+const ENCODER_STEP: i32 = TARGET_STEPS / gcd(PPR, TARGET_STEPS);
 
-const fn gcd(n: u16, m: u16) -> u16 {
+const fn gcd(n: i32, m: i32) -> i32 {
     if m == 0 { n } else { gcd(m, n % m) }
 }
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
 });
+
+pub struct QuadratureEncoderProgram<'a, PIO: Instance> {
+    prg: LoadedProgram<'a, PIO>,
+}
+
+impl<'a, PIO: Instance> QuadratureEncoderProgram<'a, PIO> {
+    pub fn new(common: &mut Common<'a, PIO>) -> Self {
+        // https://github.com/raspberrypi/pico-examples/blob/master/pio/quadrature_encoder/quadrature_encoder.pio
+        let prg = pio_asm!(
+            ".origin 0",
+            // 00 state
+            "JMP update",    // read 00
+            "JMP decrement", // read 01
+            "JMP increment", // read 10
+            "JMP update",    // read 11
+            // 01 state
+            "JMP increment", // read 00
+            "JMP update",    // read 01
+            "JMP update",    // read 10
+            "JMP decrement", // read 11
+            // 10 state
+            "JMP decrement", // read 00
+            "JMP update",    // read 01
+            "JMP update",    // read 10
+            "JMP increment", // read 11
+            // last 2 states implemented in place, become target for other jumps
+            // 11 state
+            "JMP update",    // read 00
+            "JMP increment", // read 01
+            "decrement:",
+            "JMP Y--, update", // read 10
+            // main loop begins
+            ".wrap_target",
+            "update:",
+            "MOV ISR, Y", // read 11
+            "PUSH noblock",
+            "sample_pins:",
+            "OUT ISR, 2",
+            "IN PINS, 2",
+            "MOV OSR, ISR",
+            "MOV PC, ISR",
+            "increment:",
+            "MOV Y, ~Y",
+            "JMP Y--, increment_cont",
+            "increment_cont:",
+            "MOV Y, ~Y",
+            ".wrap"
+        );
+
+        let prg = common.load_program(&prg.program);
+
+        Self { prg }
+    }
+}
+
+pub struct QuadratureEncoder<'d, T: Instance, const SM: usize> {
+    sm: StateMachine<'d, T, SM>,
+}
+
+impl<'d, T: Instance, const SM: usize> QuadratureEncoder<'d, T, SM> {
+    pub fn new(
+        pio: &mut Common<'d, T>,
+        mut sm: StateMachine<'d, T, SM>,
+        mut pin_0: Pin<'d, T>,
+        mut pin_1: Pin<'d, T>,
+        program: &QuadratureEncoderProgram<'d, T>,
+    ) -> Self {
+        pin_0.set_pull(embassy_rp::gpio::Pull::Up);
+        pin_1.set_pull(embassy_rp::gpio::Pull::Up);
+
+        let borrowed_pio_pins = [&pin_0, &pin_1];
+
+        sm.set_pin_dirs(embassy_rp::pio::Direction::In, &borrowed_pio_pins);
+
+        let mut cfg = Config::default();
+        cfg.set_in_pins(&borrowed_pio_pins);
+        cfg.set_jmp_pin(&pin_0);
+
+        cfg.use_program(&program.prg, &[]);
+
+        cfg.shift_in.auto_fill = false;
+        cfg.shift_in.direction = ShiftDirection::Left;
+        cfg.shift_in.threshold = 32;
+
+        cfg.fifo_join = FifoJoin::Duplex;
+
+        cfg.clock_divider = 1.to_fixed();
+
+        sm.set_config(&cfg);
+        sm.set_enable(true);
+
+        Self { sm }
+    }
+
+    pub async fn read(&mut self) -> i32 {
+        let num_to_purge = self.sm.rx().level();
+        for _ in 0..num_to_purge {
+            self.sm.rx().pull();
+        }
+
+        self.sm.rx().wait_pull().await as i32
+    }
+}
 
 #[embassy_executor::task]
 pub async fn encoder_task(
@@ -39,30 +156,37 @@ pub async fn encoder_task(
         mut common, sm0, ..
     } = Pio::new(pio, Irqs);
 
-    let prg = PioEncoderProgram::new(&mut common);
-    let mut encoder_0 = PioEncoder::new(&mut common, sm0, pin_0, pin_1, &prg);
+    let pin_0 = common.make_pio_pin(pin_0);
+    let pin_1 = common.make_pio_pin(pin_1);
 
-    let mut rolling_delta: i16 = 0;
+    let prg = QuadratureEncoderProgram::new(&mut common);
+    let mut encoder_0 = QuadratureEncoder::new(&mut common, sm0, pin_0, pin_1, &prg);
+
+    let mut last_value: i32 = 0;
+    let mut rolling_delta: i32 = 0;
     let mut game_reported_value: u8 = 0;
 
     loop {
-        rolling_delta += match encoder_0.read().await {
-            Direction::Clockwise => ENCODER_STEP as i16,
-            Direction::CounterClockwise => -(ENCODER_STEP as i16),
-        };
+        let new_reading = encoder_0.read().await;
 
-        if rolling_delta > THRESHOLD as i16 {
-            rolling_delta -= THRESHOLD as i16;
+        rolling_delta += (new_reading - last_value) * ENCODER_STEP;
+
+        if rolling_delta > THRESHOLD {
+            rolling_delta -= THRESHOLD;
             game_reported_value += 1;
         } else if rolling_delta < 0 {
-            rolling_delta += THRESHOLD as i16;
+            rolling_delta += THRESHOLD;
             game_reported_value -= 1;
         }
 
-        debug!(
-            "rolling {} game reported {}",
-            rolling_delta, game_reported_value
-        );
+        if last_value != new_reading {
+            debug!(
+                "threshold {} encoder_step {} raw {} rolling {} game reported {}",
+                THRESHOLD, ENCODER_STEP, new_reading, rolling_delta, game_reported_value
+            );
+        }
+
+        last_value = new_reading;
 
         output.send(game_reported_value);
     }
